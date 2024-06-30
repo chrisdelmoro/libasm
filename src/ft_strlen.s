ebc9e1ea4c176fd6230690f31c56bf3fd6e6b201
Christian C. Del Moro <christian.delmoro@protonmail.com>
2024-06-30 19:56:49 -0300

    Added comments to strlen

--- src/ft_strlen.s
+++ src/ft_strlen.s
@@ -2,11 +2,11 @@ section .text
 	global ft_strlen
 
 ft_strlen:
-	xor	rax, rax
+	xor	rax, rax ; bitwise xor operation to zero the rax register
 	loop:
-		cmp	byte [rdi + rax], 0
-		je	end_loop
-		inc	rax
-		jmp	loop
+		cmp	byte [rdi + rax], 0 ; compares each byte with 0. rdi is the reference pointer to the string, rax the counter which increments to check every character
+		je	end_loop            ; if the above comparison is true, jump to end_loop label
+		inc	rax                 ; increases the rax register which is acting as the loop counter
+		jmp	loop                ; restart the loop
 	end_loop:
 		ret
